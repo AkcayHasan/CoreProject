a3ff34228cd416cf31566686c1a43d87b26cbedd
HasanAkcay <mehmethasanakcay@gmail.com>
2021-12-12 17:12:35 +0300

    Initial commit

---
+++ app/src/main/java/com/hasankcay/coreproject/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hasankcay.coreproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
